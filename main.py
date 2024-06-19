4a3b619e90b1202cd7ebd5f590a1620feeea0e50
Satish Sahu <98869512+satishsahu25@users.noreply.github.com>
2024-06-19 22:06:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, request
+import os
 app=Flask(__name__)
 
 
-@app.route("/")
+@app.route("/",methods=['GET'])
 def hell():
     return "Hello, world!"
+
+@app.route("/ask",methods=['GET'])
+async def ask():
+    prompt=request.args
+    # result=model.invoke([prompt])
+    # return({"response":result["content"]})
+    return({"response":prompt})
