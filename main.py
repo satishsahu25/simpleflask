6c192e515a131a760de60c97bf281b9139e90f34
Satish Sahu <98869512+satishsahu25@users.noreply.github.com>
2024-06-19 22:13:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
 from flask import Flask, request
 import os
 app=Flask(__name__)
+from langchain_openai import AzureChatOpenAI
+import langchain
+from dotenv import load_dotenv
+load_dotenv()
+import pickle
+# from fastapi.middleware.cors import CORSMiddleware
+# -------PDF UPLOAD ISTARTS---------
+# from fastapi import FastAPI, File, UploadFile
+# from fastapi.responses import JSONResponse
 
+
+# # -----PDF IMPORTS STARTS--------
+from langchain_openai import AzureOpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain.chains import VectorDBQA
+from langchain_openai import AzureOpenAI
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+# -----PDF IMPORTS END--------
+openai_apiverson=os.environ["AZURE_OPENAI_VERSION"] 
+openai_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"] 
+openai_apikey=os.environ["AZURE_OPENAI_API_KEY"] 
+openai_model_name=os.environ["MODEL_DEPLOY_NAME"]
+embed_model=os.environ["EMBED_MODEL_NAME"]
+embed_deploy_name=os.environ["EMBED_DEPLOY_MODEL_NAME"]
+embed_endpoint=os.environ["EMBED_ENDPOINT"]
+
+model=AzureChatOpenAI(
+    openai_api_version=openai_apiverson,
+    azure_deployment=openai_model_name,
+    )
 
 @app.route("/",methods=['GET'])
 def hell():
@@ -10,6 +42,6 @@ def hell():
 @app.route("/ask",methods=['GET'])
 async def ask():
     prompt=request.args
-    # result=model.invoke([prompt])
-    # return({"response":result["content"]})
-    return({"response":prompt})
+    result=model.invoke([prompt])
+    return({"response":result["content"]})
+   
