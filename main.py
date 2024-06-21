973d6d8d42419e6412c13aab65380fcc4ebcb866
RajNS02 <95965558+RajNS02@users.noreply.github.com>
2024-06-21 23:03:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -130,4 +130,5 @@ async def ragindex():
 
    
 if __name__ == '__main__':
-  app.run(host="0.0.0.0", debug=True, port=8000)
+    app.debug = True
+    app.run(host="0.0.0.0", debug=True, port=8000)
