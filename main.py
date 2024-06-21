ff7d961db596b8ab1fac83e8fa37e5767964286d
Satish Sahu <98869512+satishsahu25@users.noreply.github.com>
2024-06-21 12:13:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
 app=Flask(__name__)
-from langchain_openai import AzureChatOpenAI
+# from langchain_openai import AzureChatOpenAI
 import langchain
 
 from dotenv import load_dotenv
@@ -14,106 +14,106 @@ import pickle
 # from fastapi import FastAPI, File, UploadFile
 
 # # -----PDF IMPORTS STARTS--------
-from langchain_openai import AzureOpenAIEmbeddings
-from langchain_community.vectorstores import Chroma
-from langchain.chains import VectorDBQA
-from langchain_openai import AzureOpenAI
-from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
+# from langchain_openai import AzureOpenAIEmbeddings
+# from langchain_community.vectorstores import Chroma
+# from langchain.chains import VectorDBQA
+# from langchain_openai import AzureOpenAI
+# from langchain_community.document_loaders import PyPDFLoader
+# from langchain.text_splitter import CharacterTextSplitter
+# from langchain.chains import LLMChain
+# from langchain.prompts import PromptTemplate
 
 # -----PDF IMPORTS END--------
-openai_apiverson=os.getenv("AZURE_OPENAI_VERSION") 
-openai_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT") 
-openai_apikey=os.getenv("AZURE_OPENAI_API_KEY") 
-openai_model_name=os.getenv("MODEL_DEPLOY_NAME")
-embed_model=os.getenv("EMBED_MODEL_NAME")
-embed_deploy_name=os.getenv("EMBED_DEPLOY_MODEL_NAME")
-embed_endpoint=os.getenv("EMBED_ENDPOINT")
-embed_openai_key=os.getenv("EMBED_OPENAI_KEY")
+# openai_apiverson=os.getenv("AZURE_OPENAI_VERSION") 
+# openai_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT") 
+# openai_apikey=os.getenv("AZURE_OPENAI_API_KEY") 
+# openai_model_name=os.getenv("MODEL_DEPLOY_NAME")
+# embed_model=os.getenv("EMBED_MODEL_NAME")
+# embed_deploy_name=os.getenv("EMBED_DEPLOY_MODEL_NAME")
+# embed_endpoint=os.getenv("EMBED_ENDPOINT")
+# embed_openai_key=os.getenv("EMBED_OPENAI_KEY")
 
 
-model=AzureChatOpenAI(
-    openai_api_version=openai_apiverson,
-    azure_deployment=openai_model_name,
-    )
+# model=AzureChatOpenAI(
+#     openai_api_version=openai_apiverson,
+#     azure_deployment=openai_model_name,
+#     )
 
 @app.route("/",methods=['GET'])
 async def basic():
     return {"Hello, world!":"hi"}
 
 
-@app.route("/ask",methods=['GET'])
-async def basic_ask():
-    prompt=request.args["query"]
-    result=model.invoke([prompt]).content
-    return {"response":result}
+# @app.route("/ask",methods=['GET'])
+# async def basic_ask():
+#     prompt=request.args["query"]
+#     result=model.invoke([prompt]).content
+#     return {"response":result}
 
-UPLOAD_FOLDER = './'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# UPLOAD_FOLDER = './'
+# ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-UPLOADED_FILE_NAME=""
-file_path=os.getenv("file_path")
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            with open(file_path, 'wb') as file:
-                # Serialize and write the variable to the file
-                pickle.dump("./"+filename, file)
-    return "uploaded"
+# def allowed_file(filename):
+#     return '.' in filename and \
+#            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+# UPLOADED_FILE_NAME=""
+# file_path=os.getenv("file_path")
 
-loaded_data = ""
-@app.route("/rag",methods=["GET", "POST"])
-async def rag():
-            query=request.args["query"]
-            # # loading--------------
-            # Open the file in binary mode
-            with open(os.getenv("file_path"), 'rb') as file:
-                # Deserialize and retrieve the variable from the file
-                loaded_data = pickle.load(file)
-            # print(loaded_data)
-            fileloader=PyPDFLoader(loaded_data)
-            documents=fileloader.load() 
-            # # chunking ---------
-            text_splitter=CharacterTextSplitter(chunk_size=800,chunk_overlap=20)
-            texts=text_splitter.split_documents(documents) 
-            # embeddings-------------
-            embeddings= AzureOpenAIEmbeddings(
-            model=embed_model,
-            azure_deployment=embed_deploy_name,
-            azure_endpoint=embed_endpoint,
-            openai_api_key=embed_openai_key,
-            openai_api_version=openai_apiverson
-            )
-            db=Chroma.from_documents(texts,embeddings)
-            docs = db.similarity_search(query,k=1)
-            # print(docs)
-            # text generation------------------------
-            result=model.invoke([docs[0].page_content])
-            return({"response":result.content})
+# @app.route('/upload', methods=['GET', 'POST'])
+# def upload_file():
+#     if request.method == 'POST':
+#         # check if the post request has the file part
+#         if 'file' not in request.files:
+#             flash('No file part')
+#             return redirect(request.url)
+#         file = request.files['file']
+#         # If the user does not select a file, the browser submits an
+#         # empty file without a filename.
+#         if file.filename == '':
+#             flash('No selected file')
+#             return redirect(request.url)
+#         if file and allowed_file(file.filename):
+#             filename = secure_filename(file.filename)
+#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#             with open(file_path, 'wb') as file:
+#                 # Serialize and write the variable to the file
+#                 pickle.dump("./"+filename, file)
+#     return "uploaded"
+
+
+
+# loaded_data = ""
+# @app.route("/rag",methods=["GET", "POST"])
+# async def rag():
+#             query=request.args["query"]
+#             # # loading--------------
+#             # Open the file in binary mode
+#             with open(os.getenv("file_path"), 'rb') as file:
+#                 # Deserialize and retrieve the variable from the file
+#                 loaded_data = pickle.load(file)
+#             # print(loaded_data)
+#             fileloader=PyPDFLoader(loaded_data)
+#             documents=fileloader.load() 
+#             # # chunking ---------
+#             text_splitter=CharacterTextSplitter(chunk_size=800,chunk_overlap=20)
+#             texts=text_splitter.split_documents(documents) 
+#             # embeddings-------------
+#             embeddings= AzureOpenAIEmbeddings(
+#             model=embed_model,
+#             azure_deployment=embed_deploy_name,
+#             azure_endpoint=embed_endpoint,
+#             openai_api_key=embed_openai_key,
+#             openai_api_version=openai_apiverson
+#             )
+#             db=Chroma.from_documents(texts,embeddings)
+#             docs = db.similarity_search(query,k=1)
+#             # print(docs)
+#             # text generation------------------------
+#             result=model.invoke([docs[0].page_content])
+#             return({"response":result.content})
 
 
 
