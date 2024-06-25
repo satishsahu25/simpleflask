127fb7ce70a4d93b9dbce96b883bc17ea71de17a
Satish Sahu <98869512+satishsahu25@users.noreply.github.com>
2024-06-25 13:13:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -51,77 +51,105 @@ async def hell():
 
 @app.route("/ask",methods=['GET'])
 async def ask():
-    print(request)
-    prompt=request.args["query"]
-    result=model.invoke([prompt]).content
-    return {"response":result}
+    # print(request)
+    # prompt=request.args["query"]
+    # result=model.invoke([prompt]).content
+    # return {"response":result}
+    query = request.args.get('content', default=None, type=str)
+    conversation_id = request.args.get('conversation_id', default=None, type=str)
+    user_id = request.args.get('user_id', default=None, type=str)
+    file_url = request.args.get('file_url', default=None, type=str)
+
+    if file_url is not "":
+        fileloader=PyPDFLoader(file_url)
+        documents=fileloader.load() 
+        # # chunking ---------
+        text_splitter=CharacterTextSplitter(chunk_size=800,chunk_overlap=20)
+        texts=text_splitter.split_documents(documents) 
+        # embeddings-------------
+        embeddings= AzureOpenAIEmbeddings(
+        model=embed_model,
+        azure_deployment=embed_deploy_name,
+        azure_endpoint=embed_endpoint,
+        openai_api_key=embed_openai_key,
+        openai_api_version=openai_apiverson
+        )
+        db=Chroma.from_documents(texts,embeddings)
+        docs = db.similarity_search(query,k=1)
+        # print(docs)
+        # text generation------------------------
+        result=model.invoke([docs[0].page_content])
+        return result
+    else:
+        result=model.invoke([query]).content
+        return ({"response":result.content})
 
 # # -----------------------------
 
-UPLOAD_FOLDER = './'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# UPLOAD_FOLDER = './'
+# ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+# def allowed_file(filename):
+#     return '.' in filename and \
+#            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-UPLOADED_FILE_NAME=""
-file_path=os.getenv("file_path")
+# UPLOADED_FILE_NAME=""
+# file_path=os.getenv("file_path")
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            with open(file_path, 'wb') as file:
-                # Serialize and write the variable to the file
-                pickle.dump("./"+filename, file)
-    return "uploaded"
+# @app.route('/upload', methods=['GET', 'POST'])
+# def upload_file():
+#     if request.method == 'POST':
+#         # check if the post request has the file part
+#         if 'file' not in request.files:
+#             flash('No file part')
+#             return redirect(request.url)
+#         file = request.files['file']
+#         # If the user does not select a file, the browser submits an
+#         # empty file without a filename.
+#         if file.filename == '':
+#             flash('No selected file')
+#             return redirect(request.url)
+#         if file and allowed_file(file.filename):
+#             filename = secure_filename(file.filename)
+#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#             with open(file_path, 'wb') as file:
+#                 # Serialize and write the variable to the file
+#                 pickle.dump("./"+filename, file)
+#     return "uploaded"
 
-# ----------RAG-----------------------------
+# # ----------RAG-----------------------------
 
-loaded_data = ""
-@app.route("/rag",methods=["GET", "POST"])
-async def rag():
-            query=request.args["query"]
-            query=remove_sensitive_info(query)
-            # # loading--------------
-            # Open the file in binary mode
-            with open(os.getenv("file_path"), 'rb') as file:
-                # Deserialize and retrieve the variable from the file
-                loaded_data = pickle.load(file)
-            # print(loaded_data)
-            fileloader=PyPDFLoader(loaded_data)
-            documents=fileloader.load() 
-            # # chunking ---------
-            text_splitter=CharacterTextSplitter(chunk_size=800,chunk_overlap=20)
-            texts=text_splitter.split_documents(documents) 
-            # embeddings-------------
-            embeddings= AzureOpenAIEmbeddings(
-            model=embed_model,
-            azure_deployment=embed_deploy_name,
-            azure_endpoint=embed_endpoint,
-            openai_api_key=embed_openai_key,
-            openai_api_version=openai_apiverson
-            )
-            db=Chroma.from_documents(texts,embeddings)
-            docs = db.similarity_search(query,k=1)
-            # print(docs)
-            # text generation------------------------
-            result=model.invoke([docs[0].page_content])
-            return({"response":result.content})
+# loaded_data = ""
+# @app.route("/rag",methods=["GET", "POST"])
+# async def rag():
+#             query=request.args["query"]
+#             query=remove_sensitive_info(query)
+#             # # loading--------------
+#             # Open the file in binary mode
+#             with open(os.getenv("file_path"), 'rb') as file:
+#                 # Deserialize and retrieve the variable from the file
+#                 loaded_data = pickle.load(file)
+#             # print(loaded_data)
+#             fileloader=PyPDFLoader(loaded_data)
+#             documents=fileloader.load() 
+#             # # chunking ---------
+#             text_splitter=CharacterTextSplitter(chunk_size=800,chunk_overlap=20)
+#             texts=text_splitter.split_documents(documents) 
+#             # embeddings-------------
+#             embeddings= AzureOpenAIEmbeddings(
+#             model=embed_model,
+#             azure_deployment=embed_deploy_name,
+#             azure_endpoint=embed_endpoint,
+#             openai_api_key=embed_openai_key,
+#             openai_api_version=openai_apiverson
+#             )
+#             db=Chroma.from_documents(texts,embeddings)
+#             docs = db.similarity_search(query,k=1)
+#             # print(docs)
+#             # text generation------------------------
+#             result=model.invoke([docs[0].page_content])
+#             return({"response":result.content})
 
 from rag import RAG
 
