0e90497918ea04f0d9f09a40d03c3b43c6a56847
Satish Sahu <98869512+satishsahu25@users.noreply.github.com>
2024-06-26 23:01:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ from werkzeug.utils import secure_filename
 from pii_det import remove_sensitive_info
 
 app=Flask(__name__)
-from flask_cors import CORS
-CORS(app)
-
 from langchain_openai import AzureChatOpenAI
 import langchain
 
@@ -27,6 +24,13 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
+from langchain_core.messages import HumanMessage
+
+import requests
+import io
+import pdfplumber
+
+from langchain_core.documents import Document
 
 # -----PDF IMPORTS END--------
 openai_apiverson=os.getenv("AZURE_VERSION") 
@@ -44,113 +48,64 @@ model=AzureChatOpenAI(
     api_key=openai_apikey
     )
 
+
+
+def getpdf(url):
+        # Load PDF from the BytesIO stream
+    response = requests.get(url)
+    response.raise_for_status()
+    pdf_stream = io.BytesIO(response.content)
+    documents = []
+
+    with pdfplumber.open(pdf_stream) as pdf:
+        # print("gdfsgfdf")
+        for pgno,page in enumerate(pdf.pages):
+            # Extract text or structured data from each page
+            text = page.extract_text()
+            document = Document(
+            page_content=text,
+            metadata={"source": url,"page":pgno}
+            )
+            documents.append(document)  # Each page's text is treated as a separate document
+    return documents
+
 @app.route("/")
 async def hell():
     return {"Hello, world!":"hi"}
 
+from new_download import getpdf
 
-@app.route("/ask",methods=['GET'])
-async def ask():
-    # print(request)
-    prompt=request.args["query"]
-    result=model.invoke([prompt]).content
-    return {"response":result}
-    # query = request.args.get('query', default=None, type=str)
-    # conversation_id = request.args.get('conversation_id', default=None, type=str)
-    # user_id = request.args.get('user_id', default=None, type=str)
-    # file_url = request.args.get('file_url', default=None, type=str)
 
-    # if file_url is not None:
-    #     fileloader=PyPDFLoader(file_url)
-    #     documents=fileloader.load() 
-    #     # # chunking ---------
-    #     text_splitter=CharacterTextSplitter(chunk_size=800,chunk_overlap=20)
-    #     texts=text_splitter.split_documents(documents) 
-    #     # embeddings-------------
-    #     embeddings= AzureOpenAIEmbeddings(
-    #     model=embed_model,
-    #     azure_deployment=embed_deploy_name,
-    #     azure_endpoint=embed_endpoint,
-    #     openai_api_key=embed_openai_key,
-    #     openai_api_version=openai_apiverson
-    #     )
-    #     db=Chroma.from_documents(texts,embeddings)
-    #     docs = db.similarity_search(query,k=1)
-    #     # print(docs)
-    #     # text generation------------------------
-    #     result=model.invoke([docs[0].page_content])
-    #     return result
-    # else:
-    #     result=model.invoke([query]).content
-    #     return ({"response":result.content})
-
-# # -----------------------------
-
-# UPLOAD_FOLDER = './'
-# ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-# def allowed_file(filename):
-#     return '.' in filename and \
-#            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-# UPLOADED_FILE_NAME=""
-# file_path=os.getenv("file_path")
-
-# @app.route('/upload', methods=['GET', 'POST'])
-# def upload_file():
-#     if request.method == 'POST':
-#         # check if the post request has the file part
-#         if 'file' not in request.files:
-#             flash('No file part')
-#             return redirect(request.url)
-#         file = request.files['file']
-#         # If the user does not select a file, the browser submits an
-#         # empty file without a filename.
-#         if file.filename == '':
-#             flash('No selected file')
-#             return redirect(request.url)
-#         if file and allowed_file(file.filename):
-#             filename = secure_filename(file.filename)
-#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-#             with open(file_path, 'wb') as file:
-#                 # Serialize and write the variable to the file
-#                 pickle.dump("./"+filename, file)
-#     return "uploaded"
-
-# # ----------RAG-----------------------------
-
-# loaded_data = ""
-# @app.route("/rag",methods=["GET", "POST"])
-# async def rag():
-#             query=request.args["query"]
-#             query=remove_sensitive_info(query)
-#             # # loading--------------
-#             # Open the file in binary mode
-#             with open(os.getenv("file_path"), 'rb') as file:
-#                 # Deserialize and retrieve the variable from the file
-#                 loaded_data = pickle.load(file)
-#             # print(loaded_data)
-#             fileloader=PyPDFLoader(loaded_data)
-#             documents=fileloader.load() 
-#             # # chunking ---------
-#             text_splitter=CharacterTextSplitter(chunk_size=800,chunk_overlap=20)
-#             texts=text_splitter.split_documents(documents) 
-#             # embeddings-------------
-#             embeddings= AzureOpenAIEmbeddings(
-#             model=embed_model,
-#             azure_deployment=embed_deploy_name,
-#             azure_endpoint=embed_endpoint,
-#             openai_api_key=embed_openai_key,
-#             openai_api_version=openai_apiverson
-#             )
-#             db=Chroma.from_documents(texts,embeddings)
-#             docs = db.similarity_search(query,k=1)
-#             # print(docs)
-#             # text generation------------------------
-#             result=model.invoke([docs[0].page_content])
-#             return({"response":result.content})
-
+@app.route("/ask",methods=['GET','POST'])
+def ask():
+                query = request.args.get('content', default=None, type=str)
+                # query = "what is deep work?"
+                conversation_id = request.args.get('conversation_id', default=None, type=str)
+                user_id = request.args.get('user_id', default=None, type=str)
+                file_url = request.args.get('file_url', default=None, type=str)
+                if file_url is not None:
+                    documents=getpdf(file_url)
+                    # # chunking ---------
+                    text_splitter=CharacterTextSplitter(chunk_size=800,chunk_overlap=20)
+                    texts=text_splitter.split_documents(documents) 
+                    # embeddings-------------
+                    embeddings= AzureOpenAIEmbeddings(
+                    model=embed_model,
+                    azure_deployment=embed_deploy_name,
+                    azure_endpoint=embed_endpoint,
+                    openai_api_key=embed_openai_key,
+                    openai_api_version=openai_apiverson
+                    )
+                    db=Chroma.from_documents(texts,embeddings)
+                    docs = db.similarity_search(query,k=2)
+                    print(docs)
+                    # text generation------------------------
+                    result=model.invoke([HumanMessage(content=docs[0].page_content)])
+                    return ({"response":result.content})
+                else:
+                    pass
+    
+    
 from rag import RAG
 
 @app.route("/index",methods=["GET", "POST"])
@@ -158,8 +113,6 @@ async def ragindex():
     query=request.args["query"]
     query=remove_sensitive_info(query)
     return RAG(query)  
-    # return RAG("How do I download the Control Room certificate using Google Chrome?")  
-
    
 if __name__ == '__main__':
   app.run(debug=True)
