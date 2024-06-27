36fa2d2bb34cd868dd093f2e4c0abba8d9fcf2be
Satish Sahu <98869512+satishsahu25@users.noreply.github.com>
2024-06-27 17:30:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,20 @@
 import os
-from flask import Flask, flash, request, redirect, url_for
-from werkzeug.utils import secure_filename
-
+from flask import Flask, request
 from pii_det import remove_sensitive_info
 
-app=Flask(__name__)
 from langchain_openai import AzureChatOpenAI
 import langchain
-
 from dotenv import load_dotenv
 load_dotenv()
-import pickle
 
-# -------PDF UPLOAD ISTARTS---------
-# from fastapi import FastAPI, File, UploadFile
+
+app=Flask(__name__)
+
 
 # # -----PDF IMPORTS STARTS--------
 from langchain_openai import AzureOpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
-from langchain.chains import VectorDBQA
-from langchain_openai import AzureOpenAI
-from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
 from langchain_core.messages import HumanMessage
 
 import requests
@@ -31,6 +22,10 @@ import io
 import pdfplumber
 
 from langchain_core.documents import Document
+from new_download import getpdf
+from rag import RAG
+from az_cdb import save_conversation_history
+from final_processing import construct_final_query
 
 # -----PDF IMPORTS END--------
 openai_apiverson=os.getenv("AZURE_VERSION") 
@@ -49,7 +44,6 @@ model=AzureChatOpenAI(
     )
 
 
-
 def getpdf(url):
         # Load PDF from the BytesIO stream
     response = requests.get(url)
@@ -58,7 +52,6 @@ def getpdf(url):
     documents = []
 
     with pdfplumber.open(pdf_stream) as pdf:
-        # print("gdfsgfdf")
         for pgno,page in enumerate(pdf.pages):
             # Extract text or structured data from each page
             text = page.extract_text()
@@ -69,50 +62,50 @@ def getpdf(url):
             documents.append(document)  # Each page's text is treated as a separate document
     return documents
 
+
 @app.route("/")
 async def hell():
     return {"Hello, world!":"hi"}
 
-from new_download import getpdf
-
 
 @app.route("/ask",methods=['GET','POST'])
 def ask():
-                query = request.args.get('content', default=None, type=str)
-                # query = "what is deep work?"
-                conversation_id = request.args.get('conversation_id', default=None, type=str)
+                query = request.args.get('query', default=None, type=str)
                 user_id = request.args.get('user_id', default=None, type=str)
-                file_url = request.args.get('file_url', default=None, type=str)
-                if file_url is not None:
+                file_url = request.args.get('file_url', default="", type=str)
+                if file_url != "":
                     documents=getpdf(file_url)
                     # # chunking ---------
                     text_splitter=CharacterTextSplitter(chunk_size=800,chunk_overlap=20)
                     texts=text_splitter.split_documents(documents) 
                     # embeddings-------------
                     embeddings= AzureOpenAIEmbeddings(
-                    model=embed_model,
-                    azure_deployment=embed_deploy_name,
-                    azure_endpoint=embed_endpoint,
-                    openai_api_key=embed_openai_key,
-                    openai_api_version=openai_apiverson
-                    )
-                    db=Chroma.from_documents(texts,embeddings)
-                    docs = db.similarity_search(query,k=2)
+                                                        model=embed_model,
+                                                        azure_deployment=embed_deploy_name,
+                                                        azure_endpoint=embed_endpoint,
+                                                        openai_api_key=embed_openai_key,
+                                                        openai_api_version=openai_apiverson
+                                                    )
+                    db=Chroma.from_documents(texts, embeddings)
+                    docs = db.similarity_search(query, k=2)
                     print(docs)
                     # text generation------------------------
-                    result=model.invoke([HumanMessage(content=docs[0].page_content)])
-                    return ({"response":result.content})
+                    final_query, buffer = construct_final_query(user_id, query, docs[0].page_content)
+                    final_query = "Below is the past conversation history and relevant documents retrieved from a knowledge base." + f"\n{final_query}\n If the answer is not present in the chat history or provided documents, give the answer from your own knowledge.\n So, the answer is:"
+                    result = model.invoke([HumanMessage(content=final_query)])
+                    answer = result.content
+                    # Update/save coversation history------------------------
+                    if buffer:
+                        new_entry = f"Q: {query}\nA: {answer}"
+                        buffer.append(new_entry)
+                        save_conversation_history(user_id, buffer)
+                    else:
+                        new_entry = [f"Q: {query}\nA: {answer}"]
+                        save_conversation_history(user_id, new_entry)
+                    return ({"response": answer})
                 else:
-                    pass
+                    query=remove_sensitive_info(query)
+                    return ({"response":RAG(query, user_id)})
     
-    
-from rag import RAG
-
-@app.route("/index",methods=["GET", "POST"])
-async def ragindex():
-    query=request.args["query"]
-    query=remove_sensitive_info(query)
-    return RAG(query)  
-   
 if __name__ == '__main__':
   app.run(debug=True)
