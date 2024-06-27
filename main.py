369dcc2c44e25e539d119d9d8062cf25c53c5504
RajNS02 <95965558+RajNS02@users.noreply.github.com>
2024-06-27 21:18:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,23 +44,23 @@ model=AzureChatOpenAI(
     )
 
 
-def getpdf(url):
-        # Load PDF from the BytesIO stream
-    response = requests.get(url)
-    response.raise_for_status()
-    pdf_stream = io.BytesIO(response.content)
-    documents = []
+# def getpdf(url):
+#         # Load PDF from the BytesIO stream
+#     response = requests.get(url)
+#     response.raise_for_status()
+#     pdf_stream = io.BytesIO(response.content)
+#     documents = []
 
-    with pdfplumber.open(pdf_stream) as pdf:
-        for pgno,page in enumerate(pdf.pages):
-            # Extract text or structured data from each page
-            text = page.extract_text()
-            document = Document(
-            page_content=text,
-            metadata={"source": url,"page":pgno}
-            )
-            documents.append(document)  # Each page's text is treated as a separate document
-    return documents
+#     with pdfplumber.open(pdf_stream) as pdf:
+#         for pgno,page in enumerate(pdf.pages):
+#             # Extract text or structured data from each page
+#             text = page.extract_text()
+#             document = Document(
+#             page_content=text,
+#             metadata={"source": url,"page":pgno}
+#             )
+#             documents.append(document)  # Each page's text is treated as a separate document
+#     return documents
 
 
 @app.route("/")
