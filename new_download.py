abadfc9348fcaaa3bf62e71b696d1c6c64886e71
Satish Sahu <98869512+satishsahu25@users.noreply.github.com>
2024-06-28 13:26:15 +0530

    Update new_download.py

--- new_download.py
+++ new_download.py
@@ -5,20 +5,23 @@ import pdfplumber
 from langchain_core.documents import Document
 
 def getpdf(url):
-        # Load PDF from the BytesIO stream
-    response = requests.get(url)
-    response.raise_for_status()
-    pdf_stream = io.BytesIO(response.content)
-    documents = []
+    try:
+                # Load PDF from the BytesIO stream
+        response = requests.get(url)
+        response.raise_for_status()
+        pdf_stream = io.BytesIO(response.content)
+        documents = []
 
-    with pdfplumber.open(pdf_stream) as pdf:
-        # print("gdfsgfdf")
-        for pgno,page in enumerate(pdf.pages):
-            # Extract text or structured data from each page
-            text = page.extract_text()
-            document = Document(
-            page_content=text,
-            metadata={"source": url,"page":pgno}
-            )
-            documents.append(document)  # Each page's text is treated as a separate document
-    return documents
+        with pdfplumber.open(pdf_stream) as pdf:
+            # print("gdfsgfdf")
+            for pgno,page in enumerate(pdf.pages):
+                # Extract text or structured data from each page
+                text = page.extract_text()
+                document = Document(
+                page_content=text,
+                metadata={"source": url,"page":pgno}
+                )
+                documents.append(document)  # Each page's text is treated as a separate document
+        return documents
+    except requests.RequestException as e:
+        return {'error': str(e)}, 500
