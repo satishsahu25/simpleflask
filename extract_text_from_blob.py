c8b71d695d5b1e8a9b4548083b07375b7c2676cd
Satish Sahu <98869512+satishsahu25@users.noreply.github.com>
2024-06-28 23:29:13 +0530

    Add files via upload

---
+++ extract_text_from_blob.py
@@ -0,0 +1,48 @@
+import os
+from io import BytesIO
+import pdfplumber
+from azure.storage.blob import BlobClient
+from langchain_core.documents import Document
+from dotenv import load_dotenv
+load_dotenv()
+
+# Fetching the credentials
+account_key = os.getenv('AZURE_BLOB_STORAGE_ACCOUNT_KEY')
+
+def extract_text(blob_url: str):
+    # Create a BlobClient object using the blob URL
+    blob_client = BlobClient.from_blob_url(blob_url, credential=account_key)
+
+    # Determine the type of file
+    file_type = blob_url.split('.')[-1]
+
+    # For txt file
+    if file_type=='txt':
+        text = blob_client.download_blob().readall().decode('utf-8') # Download the blob content and store it in a variable
+        blob_content = []
+        blob_content.append(Document(page_content=text, metadata={"source": blob_url}))
+        # print(blob_content)
+
+    # For pdf file
+    elif file_type=='pdf':
+        blob_pdf = blob_client.download_blob()
+        
+        stream = BytesIO()
+        blob_pdf.download_to_stream(stream)
+        blob_content = []
+
+        with pdfplumber.open(stream) as pdf:
+            for page_num, page in enumerate(pdf.pages):
+                # Extract text or structured data from each page
+                text = page.extract_text()
+                document = Document(
+                                    page_content=text,
+                                    metadata={"source": blob_url, "page": page_num}
+                                   )
+                blob_content.append(document)  # Each page's text is treated as a separate document
+    
+    # For other file types
+    else:
+        blob_content = None
+
+    return blob_content
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, request
-from pii_det import remove_sensitive_info
 
 from langchain_openai import AzureChatOpenAI
 import langchain
@@ -12,22 +11,20 @@ app=Flask(__name__)
 CORS(app)
 
 
-# # -----PDF IMPORTS STARTS--------
+#  -----PDF IMPORTS STARTS--------
 from langchain_openai import AzureOpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_core.messages import HumanMessage
 
-import requests
-import io
-import pdfplumber
-
 from langchain_core.documents import Document
 from rag import RAG
 from az_cdb import save_conversation_history
 from final_processing import construct_final_query
+from pii_det import remove_sensitive_info
+from extract_text_from_blob import extract_text
 
-# -----PDF IMPORTS END--------
+# -----PDF IMPORTS END-----------
 openai_apiverson=os.getenv("AZURE_VERSION") 
 openai_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT") 
 openai_apikey=os.getenv("AZURE_OPENAI_API_KEY") 
@@ -43,56 +40,58 @@ model=AzureChatOpenAI(
     api_key=openai_apikey
     )
 
-from new_download import getpdf
-
-@app.route("/",methods=['GET','POST'])
+@app.route("/", methods=['GET','POST'])
 def basic():
-    return {"Hello, world!":"hi"}
+    return {"Hello, world!": "hi"}
+
 
 @app.route("/ask",methods=['GET','POST'])
 def ask():
-                query = request.args.get('query', default=None, type=str)
-                user_id = request.args.get('user_id', default=None, type=str)
-                file_url = request.args.get('file_url', default="x", type=str)
-                if file_url != "x":
-                    documents=getpdf(file_url)
-                    return {"response":documents}
-                    try:
-                        # chunking ---------
-                        text_splitter=CharacterTextSplitter(chunk_size=800,chunk_overlap=20)
-                        texts=text_splitter.split_documents(documents) 
-                        # embeddings-------------
-                        embeddings= AzureOpenAIEmbeddings(
-                                                            model=embed_model,
-                                                            azure_deployment=embed_deploy_name,
-                                                            azure_endpoint=embed_endpoint,
-                                                            openai_api_key=embed_openai_key,
-                                                            openai_api_version=openai_apiverson
-                                                        )
-                        db=Chroma.from_documents(texts, embeddings)
-                        docs = db.similarity_search(query, k=2)
-                        print(docs)
-                        # text generation------------------------
-                        final_query, buffer = construct_final_query(user_id, query, docs[0].page_content)
-                        final_query = "Below is the past conversation history and relevant documents retrieved from a knowledge base." + f"\n{final_query}\n If the answer is not present in the chat history or provided documents, give the answer from your own knowledge.\n So, the answer is:"
-                        result = model.invoke([HumanMessage(content=final_query)])
-                        answer = result.content
-                        # print(answer)
-                        # Update/save coversation history------------------------
-                        if buffer:
-                            new_entry = f"Q: {query}\nA: {answer}"
-                            buffer.append(new_entry)
-                            save_conversation_history(user_id, buffer)
-                        else:
-                            new_entry = [f"Q: {query}\nA: {answer}"]
-                            save_conversation_history(user_id, new_entry)
-                        return {"response":answer}
-                    except:
-                            return {"response":"error in getting file"}
+    query = request.args.get('query', default=None, type=str)
+    user_id = request.args.get('user_id', default=None, type=str)
+    file_url = request.args.get('file_url', default="x", type=str)
+    if file_url != "x":
+        documents = extract_text(file_url)
+        # return {"response": documents}
+        try:
+            if not documents:
+                 return {"response": "Not a txt/pdf file!"}
+            else:
+                # chunking --------------
+                text_splitter = CharacterTextSplitter(chunk_size=800,chunk_overlap=20)
+                texts = text_splitter.split_documents(documents) 
+                # embeddings-------------
+                embeddings = AzureOpenAIEmbeddings(
+                                                    model=embed_model,
+                                                    azure_deployment=embed_deploy_name,
+                                                    azure_endpoint=embed_endpoint,
+                                                    openai_api_key=embed_openai_key,
+                                                    openai_api_version=openai_apiverson
+                                                )
+                db = Chroma.from_documents(texts, embeddings)
+                docs = db.similarity_search(query, k=2)
+                # print(docs)
+                # text generation------------------------
+                final_query, buffer = construct_final_query(user_id, query, docs[0].page_content)
+                final_query = "Below is the past conversation history and relevant documents retrieved from a knowledge base." + f"\n{final_query}\n If the answer is not present in the chat history or provided documents, give the answer from your own knowledge.\n So, the answer is:"
+                result = model.invoke([HumanMessage(content=final_query)])
+                answer = result.content
+                # print(answer)
+                # Update/save coversation history------------------------
+                if buffer:
+                    new_entry = f"Q: {query}\nA: {answer}"
+                    buffer.append(new_entry)
+                    save_conversation_history(user_id, buffer)
                 else:
-                        query=remove_sensitive_info(query)
-                        return {"response":RAG(query, user_id)}
-    
+                    new_entry = [f"Q: {query}\nA: {answer}"]
+                    save_conversation_history(user_id, new_entry)
+                return {"response":answer}
+        except:
+                return {"response": "error in getting file"}
+    else:
+            query = remove_sensitive_info(query)
+            return {"response": RAG(query, user_id)}
+
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", debug=True, port=8000)
