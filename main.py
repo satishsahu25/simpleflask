4186b3fe8f0cb7510e52c3b148746969c5ccc272
RajNS02 <95965558+RajNS02@users.noreply.github.com>
2024-06-28 10:51:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from pii_det import remove_sensitive_info
 
 from langchain_openai import AzureChatOpenAI
 import langchain
-# from dotenv import load_dotenv
-# load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 
 
 app=Flask(__name__)
@@ -22,7 +22,7 @@ import io
 import pdfplumber
 
 from langchain_core.documents import Document
-from new_download import getpdf
+# from new_download import getpdf
 from rag import RAG
 from az_cdb import save_conversation_history
 from final_processing import construct_final_query
@@ -44,23 +44,23 @@ model=AzureChatOpenAI(
     )
 
 
-# def getpdf(url):
-#         # Load PDF from the BytesIO stream
-#     response = requests.get(url)
-#     response.raise_for_status()
-#     pdf_stream = io.BytesIO(response.content)
-#     documents = []
+def getpdf(url):
+        # Load PDF from the BytesIO stream
+    response = requests.get(url)
+    response.raise_for_status()
+    pdf_stream = io.BytesIO(response.content)
+    documents = []
 
-#     with pdfplumber.open(pdf_stream) as pdf:
-#         for pgno,page in enumerate(pdf.pages):
-#             # Extract text or structured data from each page
-#             text = page.extract_text()
-#             document = Document(
-#             page_content=text,
-#             metadata={"source": url,"page":pgno}
-#             )
-#             documents.append(document)  # Each page's text is treated as a separate document
-#     return documents
+    with pdfplumber.open(pdf_stream) as pdf:
+        for pgno,page in enumerate(pdf.pages):
+            # Extract text or structured data from each page
+            text = page.extract_text()
+            document = Document(
+            page_content=text,
+            metadata={"source": url,"page":pgno}
+            )
+            documents.append(document)  # Each page's text is treated as a separate document
+    return documents
 
 
 @app.route("/")
