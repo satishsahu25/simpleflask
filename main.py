fb030fdf637b278e50bac37de8c88a258158f4be
RajNS02 <95965558+RajNS02@users.noreply.github.com>
2024-06-28 11:41:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -108,4 +108,4 @@ def ask():
                     return ({"response":RAG(query, user_id)})
     
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run(host="0.0.0.0", debug=True, port=8000)
