8b73960902ae8d79e9fa779a684cb8c2d200d767
Satish Sahu <98869512+satishsahu25@users.noreply.github.com>
2024-06-30 01:28:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,6 +44,26 @@ model=AzureChatOpenAI(
 def basic():
     return {"Hello, world!": "hi"}
 
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
 
 @app.route("/ask",methods=['GET','POST'])
 def ask():
@@ -72,8 +92,13 @@ def ask():
                                                 )
                 # return {"response":embeddings}
                 db = Chroma.from_documents(texts, embeddings)
-                docs = db.similarity_search(query, k=1)
-                return {"response":docs}
+                # docs = db.similarity_search(query, k=1)
+                # return {"response":docs}
+                retriever=db.as_retriever()
+                question_answer_chain = create_stuff_documents_chain(model, prompt)
+                rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+                results = rag_chain.invoke({"input": query})
+                return {"response":results.answer} 
                 # print(docs)
                 # text generation------------------------
                 final_query, buffer = construct_final_query(user_id, query, docs[0].page_content)
